2a049ffeeb586f6cf88b49268a7cf55cee267f28
SIDDIQST1 <siddiqst194@gmail.com>
2023-10-10 04:11:04 -0700

    Changed 6-size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-	printf("%lu\n", sizeof(char));
-	printf("%lu\n", sizeof(int));
-	printf("%lu\n", sizeof(long int));
-	printf("%lu\n", sizeof(long long int));
-	printf("%lu", sizeof(float));
-	return(0);
+	printf("Size of a %lu\n", sizeof(char));
+	printf("Size of an %lu\n", sizeof(int));
+	printf("Size of a %lu\n", sizeof(long int));
+	printf("Size of a %lu\n", sizeof(long long int));
+	printf("Size of a %lu", sizeof(float));
+	return (0);
 }
