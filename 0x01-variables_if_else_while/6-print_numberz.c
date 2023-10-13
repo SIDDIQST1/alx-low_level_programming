b6da5aaae129f0761dc802fe72908e5ea22b0c8b
SIDDIQST1 <siddiqst194@gmail.com>
2023-10-13 01:06:18 -0700

    Added a C file that print all base 10 numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int under10;
+
+	while (under10 < 10)
+	{
+		putchar((under10 % 10) + '0');
+
+		++under10;
+	}
+	printf("\n");
+return (0);
+}
