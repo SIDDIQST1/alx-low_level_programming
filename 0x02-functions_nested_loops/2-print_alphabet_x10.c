3f97865d56327d28ae15e0fad6ccce1472eb6620
SIDDIQST1 <siddiqst194@gmail.com>
2023-10-20 10:08:55 -0700

    Added a C file that prints the lower alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int timer = 0;
+	char lower_alpha;
+
+	while(timer <= 9)
+	{
+		for(lower_alpha = 'a'; lower_alpha <= 'z'; lower_alpha++)
+		{
+			putchar(lower_alpha);
+		}
+		putchar('\n');
+	}
+}
