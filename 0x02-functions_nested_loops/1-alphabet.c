6db14c3a2b94b597d0c6d5496b0eae9e34547ab6
SIDDIQST1 <siddiqst194@gmail.com>
2023-10-20 09:55:54 -0700

    Added a Cfile that prints the alphabet lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,24 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
+ * main - Entry point
+ *
+ * Return: Always 0
  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
 void print_alphabet(void)
 {
-	char letter;
+	char lower_alphabet;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (lower_alphabet = 'a'; lower_alphabet <= 'z'; lower_alphabet++)
 	{
-		putchar(letter);
-		putchar('\n');
+		putchar(lower_alphabet);
 	}
+putchar('\n');
+}
+int main(void)
+{
+	print_alphabet();
+	return (0);
 }
