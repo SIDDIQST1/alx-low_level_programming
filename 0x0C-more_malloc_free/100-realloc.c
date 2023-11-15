0f954be95a9a172ecbbe325711a1caf87530afa0
SIDDIQST1 <siddiqst194@gmail.com>
2023-11-15 05:17:53 -0800

    Added a c file thta reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,7 +8,7 @@
 * @old_size:is the size, in bytes, of the allocated space for ptr
 * @new_size: the new size, in bytes of the new memory block
 *
-* Return: pointer(p) allocate new size memory,  else NULL
+* Return: pointer allocate new size memory, or NULL
 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
