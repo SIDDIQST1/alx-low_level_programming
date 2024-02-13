0a28090d778b89686469a21cc16a94765d71a7c4
Siddiqst1 <siddiqst194@gmail.com>
2024-02-12 23:13:15 -0800

    Added a c file that prints exactly Programming is like building a multilingual puzzle, followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/*
+ * main - Entry point
+ *
+ * return - Always 0 (Sucess)
+ */
+#include <stdio.h>
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
